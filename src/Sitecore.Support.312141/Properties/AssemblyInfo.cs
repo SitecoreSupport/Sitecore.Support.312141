c9932c13d5c469dd77d8da7bc4b673e5cb1733fc
Paul Stupka <paul.stupka@sitecore.com>
2019-12-11 13:41:50 +0200

    base implementaion

---
+++ src/Sitecore.Support.312141/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.312141")]
+[assembly: AssemblyProduct("Sitecore.Support.312141")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Illegal characters in path error when url contains double quotes")]
+
